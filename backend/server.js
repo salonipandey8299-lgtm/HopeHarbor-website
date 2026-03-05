16a847497e8151ef8f91e1d821d24cd6371930f1
salonipandey8299-lgtmgit config --globle user.email salonipandey8299@gmail.com <salonipandey8299@gmail.com>
2026-03-05 12:37:16 +0530

    backend server.js

--- backend/server.js
+++ backend/server.js
@@ -11,7 +11,10 @@ dotenv.config();
 const app = express();
 
 // Middlewares
-app.use(cors());
+app.use(cors({
+    origin: ["http://localhost:5500", "https://hopeharbor-website-1.onrender.com"],
+    credentials: true
+}));
 app.use(express.json());
 
 // MongoDB
